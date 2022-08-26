9d56103e099c8bdafebcc93e8ae817b288be89b2
Meshael <Meshael@DESKTOP-OMUHQED>
2022-08-26 10:54:04 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection.Metadata;
+using System.Transactions;
+
+namespace Hello_world
+{
+    internal class Program
+    {
+        private static object conConsole;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            String stg = "Hello, World! (2)";
+            Console.WriteLine(stg);
+
+            Console.WriteLine("User Name");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Enter User's age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Username is:" + userName + "and the age is: " + age);
+        } 
+    }
+}
